5c7ccb579e0d2fbea16c454a4bb98fe882b92d5d
Nicolas Getto <Nicolas.Getto@cas.de>
2020-07-24 10:26:20 +0200

    Fix Bug and Reformat

--- src/main/java/de/cas/jaegerdemo/UserController.java
+++ src/main/java/de/cas/jaegerdemo/UserController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -22,39 +23,43 @@ import de.cas.jaegerdemo.repository.UserRepository;
 @RequestMapping("/")
 public class UserController {
 
-	@Autowired
-	private UserRepository userRepository;
+    private final RestTemplate restTemplate;
 
-	@Autowired
-	private RestTemplate restTemplate;
+    @Autowired
+    private UserRepository userRepository;
 
-	@GetMapping("/users")
-	public List<User> getAllUsers() {
-		return userRepository.findAll();
-	}
+    @Autowired
+    private UserController(RestTemplateBuilder builder) {
+        this.restTemplate = builder.build();
+    }
 
-	@GetMapping("/users/{id}")
-	public ResponseEntity<User> getUsersById(@PathVariable(value = "id") Long userId) throws UserNotFoundException {
-		User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("User not found on :: " + userId));
-		return ResponseEntity.ok().body(user);
-	}
+    @GetMapping("/users")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 
-	@PostMapping("/users")
-	public User createUser(@Validated @RequestBody User user) {
-		return userRepository.save(user);
-	}
+    @GetMapping("/users/{id}")
+    public ResponseEntity<User> getUsersById(@PathVariable(value = "id") Long userId) throws UserNotFoundException {
+        User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("User not found on :: " + userId));
+        return ResponseEntity.ok().body(user);
+    }
 
-	@DeleteMapping("/users/{id}")
-	public Map<String, Boolean> deleteUser(@PathVariable(value = "id") Long userId) throws UserNotFoundException {
-		User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("User not found on :: " + userId));
-		userRepository.delete(user);
-		Map<String, Boolean> response = new HashMap<>();
-		response.put("deleted", Boolean.TRUE);
-		return response;
-	}
+    @PostMapping("/users")
+    public User createUser(@Validated @RequestBody User user) {
+        return userRepository.save(user);
+    }
 
-	@GetMapping
-	public ResponseEntity<User> getChainedUser(@PathVariable(value = "id") Long userId) throws UserNotFoundException {
-		return restTemplate.getForEntity("http://localhost:8105/users/{id}", User.class);
-	}
+    @DeleteMapping("/users/{id}")
+    public Map<String, Boolean> deleteUser(@PathVariable(value = "id") Long userId) throws UserNotFoundException {
+        User user = userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("User not found on :: " + userId));
+        userRepository.delete(user);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+
+    @GetMapping("/users/chain/{id}")
+    public ResponseEntity<User> getChainedUser(@PathVariable(value = "id") Long userId) throws UserNotFoundException {
+        return restTemplate.getForEntity("http://localhost:8105/users/{id}", User.class);
+    }
 }
